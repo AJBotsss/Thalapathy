4d68d686d0d26063bba9086b5cf0ec4627455f69
AJBotsss <162437079+AJBotsss@users.noreply.github.com>
2024-03-07 07:48:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,8 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '22318866'))
 API_HASH = environ.get('API_HASH', '7fcac00bc99a1fb23b7563115fd079a6')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6635899847:AAH9iSG24XB0KY8FKvFP8U2dqSse1uIRSv8")
-
+BOT_TOKEN = environ.get('BOT_TOKEN', '6635899847:AAH9iSG24XB0KY8FKvFP8U2dqSse1uIRSv8')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
